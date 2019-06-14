8806d9f80aecc6ce8dfb0bef8e805b3ef65d45c4
lainglibao <lianglibao1235@gmail.com>
2019-06-14 20:29:19 +0800

    update YYModelPlus

--- Example/YYModelPlus/YYViewController.m
+++ Example/YYModelPlus/YYViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "YYViewController.h"
+#import "YYWeiboModel.h"
 
 @interface YYViewController ()
-
+@property (strong, nonatomic) NSDictionary *weiboJson;
 @end
 
 @implementation YYViewController
@@ -17,8 +18,52 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    /// get json data
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"weibo" ofType:@"json"];
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    self.weiboJson = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+    
+    [self yyModelDeserializationAction];
 }
+
+- (void)yyModelDeserializationAction {
+    /// Benchmark
+    int count = 1000;
+    NSTimeInterval begin, end;
+    begin = CACurrentMediaTime();
+    @autoreleasepool {
+        for (int i = 0; i < count; i++) {
+            [YYWeiboStatus yy_modelWithJSON:self.weiboJson];
+        }
+    }
+    end = CACurrentMediaTime();
+    printf("YYModel:         %8.2f   ", (end - begin) * 1000);
+    
+    
+    YYWeiboStatus *feed = [YYWeiboStatus yy_modelWithJSON:self.weiboJson];
+    begin = CACurrentMediaTime();
+    @autoreleasepool {
+        for (int i = 0; i < count; i++) {
+            [feed yy_modelToJSONObject];
+        }
+    }
+    end = CACurrentMediaTime();
+    if ([NSJSONSerialization isValidJSONObject:[feed yy_modelToJSONObject]]) {
+        printf("%8.2f   ", (end - begin) * 1000);
+    } else {
+        printf("   error   ");
+    }
+    
+    begin = CACurrentMediaTime();
+    @autoreleasepool {
+        for (int i = 0; i < count; i++) {
+            [NSKeyedArchiver archivedDataWithRootObject:feed];
+        }
+    }
+    end = CACurrentMediaTime();
+    printf("%8.2f\n", (end - begin) * 1000);
+}
+
 
 - (void)didReceiveMemoryWarning
 {
